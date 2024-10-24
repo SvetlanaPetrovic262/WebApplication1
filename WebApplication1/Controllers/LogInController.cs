8caf889927dd11394655b7a78a61328ddbfcc2f5
SvetlanaPetrovic262 <svetlanasovljanski@gmail.com>
2024-10-24 21:41:22 +0200

    LogInController

---
+++ WebApplication1/Controllers/LogInController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
